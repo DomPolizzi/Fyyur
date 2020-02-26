217dcc5a5184726a154b61f4024f24a26f89975f
Dominic Polizzi <ravyn@desktop-gpbqst8.thetechresource.local>
2020-02-25 20:52:10 -0500

    re-enable models.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from logging import Formatter, FileHandler
 from flask_wtf import Form
 from flask_migrate import Migrate
 from forms import ShowForm, VenueForm, ArtistForm
-# from models import Venue, Artist, Show
+from models import *
 import sys
 from sqlalchemy.dialects.postgresql import ARRAY
 #----------------------------------------------------------------------------#
@@ -30,49 +30,7 @@ migrate = Migrate(app, db)
 # Models.
 #----------------------------------------------------------------------------#
 
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    genres = db.Column(db.String())
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    website = db.Column(db.String)
-    facebook_link = db.Column(db.String(120))
-    seeking_talent = db.Column(db.String())
-    seeking_description = db.Column(db.String())
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'    
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String())
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String)
-    seeking_venue = db.Column(db.String())
-    seeking_description = db.Column(db.String())
-
-class Show(db.Model):
-    __tablename__ = 'Show'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    start_time = db.Column(db.DateTime)
-    artist_id = db.Column(db.Integer, db.ForeignKey("Artist.id"), nullable=False)
-    artist_name = db.relationship('Artist')
-    venue_id = db.Column(db.Integer, db.ForeignKey("Venue.id"), nullable=False)
-    venue_name = db.relationship('Venue')
-    start_time = db.Column(db.DateTime())
-
+# Located in models.py
 
 #----------------------------------------------------------------------------#
 # Filters.
--- models.py
+++ models.py
@@ -1,13 +1,4 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_moment import Moment
-
-app = Flask(__name__)
-moment = Moment(app)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+from app import db
 
 #----------------------------------------------------------------------------#
 # Models.
@@ -24,34 +15,34 @@ class Venue(db.Model):
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
-    website_link = db.Column(db.String())
+    website = db.Column(db.String)
     facebook_link = db.Column(db.String(120))
-    seeking_Artist = db.Column(db.Boolean, default=False)
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
+    seeking_talent = db.Column(db.String())
+    seeking_description = db.Column(db.String())
 
 class Artist(db.Model):
-    __tablename__ = 'Artist'
+    __tablename__ = 'Artist'    
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.String())
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, default=False)
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
+    website = db.Column(db.String)
+    seeking_venue = db.Column(db.String())
+    seeking_description = db.Column(db.String())
 
 class Show(db.Model):
     __tablename__ = 'Show'
 
-    show_id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey(Artist.id), nullable=False)
-    venue_id = db.Column(db.Integer, db.ForeignKey(Venue.id), nullable=False)
-    show_time = db.Column(db.DateTime())
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    start_time = db.Column(db.DateTime)
+    artist_id = db.Column(db.Integer, db.ForeignKey("Artist.id"), nullable=False)
+    artist_name = db.relationship('Artist')
+    venue_id = db.Column(db.Integer, db.ForeignKey("Venue.id"), nullable=False)
+    venue_name = db.relationship('Venue')
+    start_time = db.Column(db.DateTime())
